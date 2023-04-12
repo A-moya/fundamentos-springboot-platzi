f1733c4d9f869507fe6be69c4711ee3c879f57fb
Alex moya <82002189+A-moya@users.noreply.github.com>
2023-04-11 19:18:57 -0500

    firts commit

---
+++ src/main/java/com/fundamentos/springboot/platzi/fuundamentos/FuundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.platzi.fuundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springboot/platzi/fuundamentos/FuundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.platzi.fuundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
